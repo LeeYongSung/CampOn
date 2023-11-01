42c31168d5a66016bca9d1f6002b47e1cf3c5a87
Koomaxx86 <bluejjw86@daum.net>
2023-11-01 18:09:18 +0900

    11/01 상품등록(진행중)

--- campon/src/main/java/com/camp/campon/service/ProductServiceImpl.java
+++ campon/src/main/java/com/camp/campon/service/ProductServiceImpl.java
@@ -2,10 +2,15 @@ package com.camp.campon.service;
 
 import org.springframework.stereotype.Service;
 
+import com.camp.campon.dto.Product;
+
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Service
-public class ProductServiceImpl {
+public class ProductServiceImpl implements ProductService {@Override
+    public int insert(Product product) throws Exception {
+        ;
+    }
     
 }
