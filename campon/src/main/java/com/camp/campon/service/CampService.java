286cf891df375bd23b0150f690c0bbae0dd54a88
SSSUUUUBBB <rlatnqls1640@gamil.com>
2023-11-01 17:20:42 +0900

    subin

--- campon/src/main/java/com/camp/campon/service/CampService.java
+++ campon/src/main/java/com/camp/campon/service/CampService.java
@@ -1,5 +1,9 @@
 package com.camp.campon.service;
 
+import java.util.List;
+
+import com.camp.campon.dto.Camp;
+
 public interface CampService {
-    
+    public List<Camp> newList() throws Exception;
 }
