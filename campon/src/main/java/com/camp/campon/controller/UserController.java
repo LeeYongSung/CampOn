e53c1019058f22989621423a86f1a233e24597bb
chuyj15 <dlwlsla15@gmail.com>
2023-11-01 11:25:03 +0900

    test

--- campon/src/main/java/com/camp/campon/controller/UserController.java
+++ campon/src/main/java/com/camp/campon/controller/UserController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 @Slf4j
@@ -17,6 +19,12 @@ public class UserController {
     public String userMyPage(Model model) {
         return "user/index";
     }
+
+    @GetMapping(value="/login")
+    public String userLogin(Model model) {
+        return "user/login";
+    }
+    
     
 
 }
