56ab5c52bba95d6b27f7b56c8c8110ddff0b67df
Koomaxx86 <bluejjw86@daum.net>
2023-11-02 17:02:19 +0900

    11/02 업로드

--- campon/src/main/java/com/camp/campon/controller/ProductController.java
+++ campon/src/main/java/com/camp/campon/controller/ProductController.java
@@ -1,9 +1,13 @@
 package com.camp.campon.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.camp.campon.service.ProductService;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -11,16 +15,21 @@ import lombok.extern.slf4j.Slf4j;
 @Controller
 @RequestMapping("/product")
 public class ProductController {
+
+    @Autowired
+    private ProductService productService;
     
+    // 메인페이지
     @GetMapping("/index")
     public String productMain(Model model) {
         return "product/index";
     }
 
     // 상품등록
-     @GetMapping("/product_add")
+     @PostMapping("/productadd")
     public String productAdd(Model model) {
-        return "product/product_add";
+        return "product/productadd";
     }
+    
     
 }
