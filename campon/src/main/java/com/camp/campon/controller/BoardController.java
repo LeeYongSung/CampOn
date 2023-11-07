8d01f1664a70a416ed84e3a266b04a523357dc65
SSSUUUUBBB <rlatnqls1640@gamil.com>
2023-11-07 18:06:32 +0900

    11071806

--- campon/src/main/java/com/camp/campon/controller/BoardController.java
+++ campon/src/main/java/com/camp/campon/controller/BoardController.java
@@ -9,10 +9,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.camp.campon.dto.Board;
+import com.camp.campon.dto.Camp;
 import com.camp.campon.service.BoardService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 
 @Slf4j
@@ -48,6 +53,86 @@ public class BoardController {
         model.addAttribute("prread", prread);
         return "board/prread";
     }
+    
+    // 캠핑 리뷰 등록
+    @GetMapping(value="/crinsert")
+    public String crinsert(Model model, int reservationNo) throws Exception{
+        Camp reservation = boardService.reservation(reservationNo);
+        model.addAttribute("reservation", reservation);
+        return "board/crinsert";
+    }
+
+    @PostMapping(value="/crinsert")
+    public String crinsertPro(@ModelAttribute Board board) throws Exception {
+        int result = boardService.crinsert(board);
+        int reservationNo = board.getReservationNo();
+        if(result == 0) return "board/crinsert?reservationNo=" + reservationNo;
+        return "redirect:/board/index";
+    }
+    
+    // 상품 리뷰 등록
+    @GetMapping(value="/prinsert")
+    public String prinsert(Model model, int orderNo) throws Exception{
+        Board order = boardService.order(orderNo);
+        model.addAttribute("order", order);
+        return "board/prinsert";
+    }
+
+    @PostMapping(value="/prinsert")
+    public String prinsertPro(@ModelAttribute Board board) throws Exception {
+        int result = boardService.prinsert(board);
+        int orderNo = board.getOrderNo();
+        if(result == 0) return "board/prinsert?orderNo=" + orderNo;
+        return "redirect:/board/index";
+    }
+
+    // 캠핑 리뷰 수정
+    @GetMapping(value="/crupdate")
+    public String crupdate(int reviewNo, Model model) throws Exception {
+        Board crread = boardService.crread(reviewNo);
+        model.addAttribute("review", crread);
+        return "board/crupdate";
+    }
+    @PostMapping(value="/crupdate")
+    public String crupdatePro(@ModelAttribute Board board) throws Exception {
+        int result = boardService.crupdate(board);
+        int reviewNo = board.getReviewNo();
+        if(result == 0) return "board/crupdate?reviewNo=" + reviewNo;
+        return "redirect:/board/crread?reviewNo="+ reviewNo;
+    }
+    //캠핑리뷰 삭제
+    @PostMapping(value="/crdelete")
+    public String crdelete(int reviewNo) throws Exception {
+        int result = boardService.crdelete(reviewNo);
+        if(result == 0) return "board/crupdate?reviewNo=" + reviewNo;
+        return "redirect:/board/index";
+    }
+
+    //상품 리뷰 수정
+    @GetMapping(value="/prupdate")
+    public String prupdate(int prNo, Model model) throws Exception {
+        Board pr = boardService.prread(prNo);
+        model.addAttribute("pr", pr);
+        return "board/prupdate";
+    }
+    @PostMapping(value="/prupdate")
+    public String prupdatePro(@ModelAttribute Board board) throws Exception {
+        int result = boardService.prupdate(board);
+        int prNo = board.getPrNo();
+        if(result == 0) return "board/prupdate?prNo=" + prNo;
+        return "redirect:/board/prread?prNo="+ prNo;
+    }
+    //캠핑리뷰 삭제
+    @PostMapping(value="/prdelete")
+    public String prdelete(int prNo) throws Exception {
+        int result = boardService.prdelete(prNo);
+        if(result == 0) return "board/prupdate?prNo=" + prNo;
+        return "redirect:/board/index";
+    }
+    
+    
+    
+    
     
     
 }
