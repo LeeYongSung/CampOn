e2dce9103b78b799ed8ef576f236ab418d3e3d78
Koomaxx86 <bluejjw86@daum.net>
2023-11-11 19:45:20 +0900

    11/11 1945

--- campon/src/main/java/com/camp/campon/controller/AdminController.java
+++ campon/src/main/java/com/camp/campon/controller/AdminController.java
@@ -39,12 +39,11 @@ import com.camp.campon.service.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestBody;
 
-
 @Slf4j
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
-    
+
     @Autowired
     private UserService userService;
 
@@ -56,72 +55,79 @@ public class AdminController {
 
     @Autowired
     private BoardService boardService;
-    
+
     @Autowired
     private AdService adService;
 
-    @GetMapping(value="/index")
+    @GetMapping(value = "/index")
     public String adminUser(Model model) {
         return "admin/index";
     }
-    /********************************************** 상품 시작 ***********************************************/
-    //(관리자) 상품 관리 페이지
-    @GetMapping(value="/productlist")
+
+    /**********************************************
+     * 상품 시작
+     ***********************************************/
+    // (관리자) 상품 관리 페이지
+    @GetMapping(value = "/productlist")
     public String productList(Model model) throws Exception {
         List<Product> productList = productService.getProductList();
         model.addAttribute("productList", productList);
         return "admin/productlist";
 
     }
-    
+
     // 상품등록 페이지
     @GetMapping("/productadd")
     public String productAdd() {
         return "admin/productadd";
     }
-    
+
     // 상품등록 실행
     @PostMapping("/productInsert")
     public String productInsert(Product product) throws Exception {
         int result = productService.productInsert(product);
         log.info("상품등록 성공여부 : " + result);
-            return "redirect:/admin/productlist";
+        return "redirect:/admin/productlist";
     }
-    //상품 수정 페이지
-    @GetMapping(value="/productupdate", params="productNo")
-    public String productUpdate(@RequestParam String productNo,  Model model) throws Exception {
-        Product product = productService.selectUpd( Integer.parseInt(productNo) );
+
+    // 상품 수정 페이지
+    @GetMapping(value = "/productupdate", params = "productNo")
+    public String productUpdate(@RequestParam String productNo, Model model) throws Exception {
+        Product product = productService.selectUpd(Integer.parseInt(productNo));
         model.addAttribute("product", product);
         return "admin/productupdate";
     }
-    //상품 수정 pro
-    @PostMapping(value="/productUpdate")
+
+    // 상품 수정 pro
+    @PostMapping(value = "/productUpdate")
     public String productUpdate(Product product) throws Exception {
-        log.info("썸네일 있냐?"+ product.getProductThmFile().size());
+        log.info("썸네일 있냐?" + product.getProductThmFile().size());
         int result = productService.productUpdate(product);
-        log.info("상품수정 성공여부 : " +result);
-        return "redirect:/admin/productlist" ;
+        log.info("상품수정 성공여부 : " + result);
+        return "redirect:/admin/productlist";
     }
-    //이미지 불러오기
-    @GetMapping(value="/img", params="file")
-    public void img(@RequestParam("file") String file, HttpServletResponse response) throws Exception  {
+
+    // 이미지 불러오기
+    @GetMapping(value = "/img", params = "file")
+    public void img(@RequestParam("file") String file, HttpServletResponse response) throws Exception {
         HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.IMAGE_JPEG); 
-        headers.add("Content-Disposition", "inline;"); 
+        headers.setContentType(MediaType.IMAGE_JPEG);
+        headers.add("Content-Disposition", "inline;");
         String filePath = file;
         FileInputStream fis = new FileInputStream(filePath);
-        ServletOutputStream sos =  response.getOutputStream();
+        ServletOutputStream sos = response.getOutputStream();
         FileCopyUtils.copy(fis, sos);
     }
-    //상품 삭제 pro
-    @GetMapping(value="/delete", params="productNo")
+
+    // 상품 삭제 pro
+    @GetMapping(value = "/delete", params = "productNo")
     public String productDelete(@RequestParam String productNo) throws NumberFormatException, Exception {
         int result = productService.deleteProduct(Integer.parseInt(productNo));
         return "redirect:/admin/productlist";
     }
 
-    //멤버 관리 페이지
-    @GetMapping(value="/memberList")
+    // 멤버 관리 페이지
+    @GetMapping(value = "/memberList")
     public String memberList(Model model) throws Exception {
         List<Users> userList = userService.memberList("ROLE_USER");
         List<Users> sellList = userService.memberList("ROLE_SELL");
@@ -130,30 +136,34 @@ public class AdminController {
         return "admin/memberList";
     }
 
-    @GetMapping(value="/memberDelete")
+    @GetMapping(value = "/memberDelete")
     public String memberDelete(String userId) throws Exception {
         int result = userService.delete(userId);
-        log.info("회원 삭제 여부 : " +result);
+        log.info("회원 삭제 여부 : " + result);
         return "redirect:/admin/memberList";
     }
 
-    /********************************************** 상품 끝 ***********************************************/
+    /**********************************************
+     * 상품 끝
+     ***********************************************/
 
-    /********************************************** 캠핑장 시작 ***********************************************/
+    /**********************************************
+     * 캠핑장 시작
+     ***********************************************/
 
-    @GetMapping(value="/campproductlist")
+    @GetMapping(value = "/campproductlist")
     public String campList(Model model, Authentication auth) throws Exception {
         String name = "";
-        if(auth != null) {
+        if (auth != null) {
             name = auth.getName();
         } else {
             name = "anonymouseUser";
         }
         log.info("name : " + name);
-        if(name == null) {
+        if (name == null) {
             return "redirect:/user/login";
         } else {
-            if(!name.equals("anonymouseUser")) {
+            if (!name.equals("anonymouseUser")) {
                 // 유저 정보 획득
                 Users user = userService.selectById(name);
                 int userNo = user.getUserNo();
@@ -161,36 +171,38 @@ public class AdminController {
                 // 획득한 유저 번호로 캠핑장 정보 획득
                 List<Camp> camp = campService.campproductUser(userNo);
                 // for (Camp campEl : camp) {
-                    // campEl.getDetailList(campService.campdetailUser(campEl.getCampNo()));
+                // campEl.getDetailList(campService.campdetailUser(campEl.getCampNo()));
                 // }
                 // List<Camp> campdetailList = null;
                 // // campdetailList = null;
                 // // log.info("camp" + camp);
-                
+
                 // for(int i = 0; i < camp.size(); i++) {
-                //     int campNo = camp.get(i).getCampNo();
-                //     campdetailList = campService.campdetailUser(campNo);
-                    
-                //     log.info("campdetailList : " + campdetailList);
-                    
-                //     System.out.println();
-                //     if(campdetailList != null) campdetailList.addAll(campdetailList);
+                // int campNo = camp.get(i).getCampNo();
+                // campdetailList = campService.campdetailUser(campNo);
+
+                // log.info("campdetailList : " + campdetailList);
+
+                // System.out.println();
+                // if(campdetailList != null) campdetailList.addAll(campdetailList);
                 // }
                 // model.addAttribute("campdetailList", campdetailList);
                 model.addAttribute("campList", camp);
                 return "admin/campproductlist";
-            } 
+            }
             return "redirect:/user/login";
         }
     }
+
     /**
      * 캠핑장 등록
+     * 
      * @param camp
      * @return
      * @throws Exception
      */
-    @GetMapping(value="/campproductadd")
-    public String campInsert(@ModelAttribute Camp camp) throws Exception{
+    @GetMapping(value = "/campproductadd")
+    public String campInsert(@ModelAttribute Camp camp) throws Exception {
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
 
         String userId = auth.getName();
@@ -204,65 +216,73 @@ public class AdminController {
 
     /**
      * 캠핑장 등록처리
+     * 
      * @param camp
      * @param facilityTypeNo
      * @return
      * @throws Exception
      */
-    @PostMapping(value="/campproductadd")
+    @PostMapping(value = "/campproductadd")
     public String campInsertPro(@ModelAttribute Camp camp, @RequestParam List<String> facilityTypeNo) throws Exception {
-        
+
         int result = campService.campInsert(camp, facilityTypeNo);
 
-        if(result == 0) return "admin/campproductadd";
+        if (result == 0)
+            return "admin/campproductadd";
 
         return "redirect:/admin/campproductlist";
     }
-    
+
     /**
      * 캠핑장 수정
+     * 
      * @return
      */
-    @GetMapping(value="/campproductupdate")
+    @GetMapping(value = "/campproductupdate")
     public String campUpdate() {
         return "admin/campproductupdate";
     }
-    
-    //캠핑상품 등록
-    @GetMapping(value="/campdetailinsert")
-    public String campdetailinsert(Model model, Integer campNo, Integer userNo, @ModelAttribute Camp camp) throws Exception{
+
+    // 캠핑상품 등록
+    @GetMapping(value = "/campdetailinsert")
+    public String campdetailinsert(Model model, Integer campNo, Integer userNo, @ModelAttribute Camp camp)
+            throws Exception {
         model.addAttribute("userNo", userNo);
         model.addAttribute("campNo", campNo);
         return "admin/campdetailinsert";
     }
-    //캠핑상품 등록처리
-    @PostMapping(value="/campdetailinsert")
+
+    // 캠핑상품 등록처리
+    @PostMapping(value = "/campdetailinsert")
     public String campdetailinsertPro(@ModelAttribute Camp camp) throws Exception {
         int result = campService.detailinsert(camp);
         int campNo = camp.getCampNo();
         int userNo = camp.getUserNo();
-        
-        if(result == 0) return "redirect:/admin/campdetailinsert?campNo=" + campNo + "& userNo=" + userNo;
+
+        if (result == 0)
+            return "redirect:/admin/campdetailinsert?campNo=" + campNo + "& userNo=" + userNo;
 
         return "redirect:/admin/campproductlist";
     }
-    
-    //캠핑상품 수정
-    @GetMapping(value="/campdetailupdate")
-    public String campdetailupdate(Model model, int cpdtNo) throws Exception{
+
+    // 캠핑상품 수정
+    @GetMapping(value = "/campdetailupdate")
+    public String campdetailupdate(Model model, int cpdtNo) throws Exception {
         Camp camp = campService.productintro(cpdtNo);
         model.addAttribute("camp", camp);
         return "admin/campdetailupdate";
     }
-    //캠핑상품 수정처리
-    @PostMapping(value="/campdetailupdate")
-    public String campdetailupdatePro(Camp camp) throws Exception{
+
+    // 캠핑상품 수정처리
+    @PostMapping(value = "/campdetailupdate")
+    public String campdetailupdatePro(Camp camp) throws Exception {
         log.info("cpdtPriceStr : " + camp.getCpdtPriceStr());
         log.info("cpdtPrice : " + camp.getCpdtPrice());
 
         log.info(camp.getCpdtPrice() + "");
         // String cpdtPriceStr = camp.getCpdtPriceStr();
-        // Integer cpdtPrice = cpdtPriceStr == null ? 0 : Integer.parseInt(cpdtPriceStr);
+        // Integer cpdtPrice = cpdtPriceStr == null ? 0 :
+        // Integer.parseInt(cpdtPriceStr);
         // camp.setCpdtPrice( cpdtPrice );
 
         // int filedelete = campService.cpdidelete(camp.getCpdtNo());
@@ -272,54 +292,68 @@ public class AdminController {
 
         return "redirect:/admin/campproductlist";
     }
-    //캠핑상품 삭제처리
-    @PostMapping(value="/campdetaildelete")
-    public String campdetaildelete(int cpdtNo) throws Exception{
-        log.info("숫자 : "+ cpdtNo);
+
+    // 캠핑상품 삭제처리
+    @PostMapping(value = "/campdetaildelete")
+    public String campdetaildelete(int cpdtNo) throws Exception {
+        log.info("숫자 : " + cpdtNo);
         int filedelete = campService.cpdidelete(cpdtNo);
         int result = campService.detaildelete(cpdtNo);
-        if(result == 0) return "redirect:/admin/campdetailupdate?cpdtNo=" + cpdtNo;
+        if (result == 0)
+            return "redirect:/admin/campdetailupdate?cpdtNo=" + cpdtNo;
         return "redirect:/admin/campproductlist";
     }
-    @GetMapping(value="/campdetaildelete")
-    public String campdetaildeletepro(int cpdtNo) throws Exception{
-        log.info("숫자 : "+ cpdtNo);
+
+    @GetMapping(value = "/campdetaildelete")
+    public String campdetaildeletepro(int cpdtNo) throws Exception {
+        log.info("숫자 : " + cpdtNo);
         int filedelete = campService.cpdidelete(cpdtNo);
         int result = campService.detaildelete(cpdtNo);
-        if(result == 0) return "redirect:/admin/campdetailupdate?cpdtNo=" + cpdtNo;
+        if (result == 0)
+            return "redirect:/admin/campdetailupdate?cpdtNo=" + cpdtNo;
         return "redirect:/admin/campproductlist";
     }
-    
-    /********************************************** 캠핑장 끝 ***********************************************/
-    /********************************************** 광고 시작 ***********************************************/
-    
-        // 광고 등록
-        @GetMapping("/adinsert")
-        public String adinsert(Model model, int campNo) {
-            model.addAttribute("campNo", campNo);
-            return "admin/adinsert";
-        }
-    
-        // 광고 등록처리
-        // @PostMapping(value="/addinsert")
 
+    /**********************************************
+     * 캠핑장 끝
+     ***********************************************/
+    /**********************************************
+     * 광고 시작
+     ***********************************************/
 
-    @GetMapping(value="/adlist")
+    // 광고 등록 페이지
+    @GetMapping("/adinsert")
+    public String adinsert(Model model, int campNo) {
+        model.addAttribute("campNo", campNo);
+        return "admin/adinsert";
+    }
+
+    // 광고 등록 실행
+    @PostMapping(value = "/addinsertpro")
+    public String adinsertpro(@ModelAttribute Ad ad) throws Exception {
+        int result = adService.adinsert(ad);
+        return "redirect:admin/adlistseller";
+    }
+
+    @GetMapping(value = "/adlist")
     public String adlist(Model model) throws Exception {
         List<Ad> adlist = adService.adlist();
         model.addAttribute("adlist", adlist);
         return "admin/adlist";
     }
-    @GetMapping(value="/adcheck")
+
+    @GetMapping(value = "/adcheck")
     public String adcheck(int adNo) throws Exception {
         int result = adService.adcheck(adNo);
         return "redirect:/admin/adlist";
     }
-    @GetMapping(value="/adlistseller")
+
+    @GetMapping(value = "/adlistseller")
     public String adlistseller(Model model, Principal principal) throws Exception {
         int userNo = 0;
-        if (principal == null){ userNo = 1000;}
-        else {
+        if (principal == null) {
+            userNo = 1000;
+        } else {
             String userId = principal.getName();
             Users users = userService.selectById(userId);
             userNo = users.getUserNo();
@@ -328,13 +362,5 @@ public class AdminController {
         model.addAttribute("adlistseller", adlistseller);
         return "admin/adlistseller";
     }
-    
-    
-    
-        @PostMapping(value="/addinsertpro")
-        public String adinsertpro(@ModelAttribute Ad ad){
-            
 
-            return "redirect:admin/adlistseller";
-        }
 }
--- campon/src/main/java/com/camp/campon/service/AdService.java
+++ campon/src/main/java/com/camp/campon/service/AdService.java
@@ -8,9 +8,14 @@ public interface AdService {
     
    // 광고 리스트
    public List<Ad> adlist() throws Exception;
+
    // 승인
    public int adcheck(int adNo) throws Exception;
+
    // 광고리스트 (seller)
    public List<Ad> adlistseller(int userNo) throws Exception;
+
+   // 광고등록
+   public int adinsert(Ad ad) throws Exception;
 
 }
