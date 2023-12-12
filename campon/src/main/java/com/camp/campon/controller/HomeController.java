ac7be6d1f5dcc1b2cc9cb5ded2db434b2e5bedf5
wwwalohacampus <h850415@naver.com>
2023-12-12 18:20:52 +0900

    ✅

--- campon/src/main/java/com/camp/campon/controller/HomeController.java
+++ campon/src/main/java/com/camp/campon/controller/HomeController.java
@@ -1,29 +1,33 @@
 package com.camp.campon.controller;
 
+import java.io.File;
 import java.io.FileInputStream;
+import java.io.InputStream;
 import java.util.List;
 
 import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.FileCopyUtils;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
 
 import com.camp.campon.dto.Board;
 import com.camp.campon.dto.Camp;
 import com.camp.campon.service.BoardService;
 import com.camp.campon.service.CampService;
 import com.camp.campon.service.UserService;
+
+import lombok.extern.slf4j.Slf4j;
 
 
 @Slf4j
@@ -76,8 +80,27 @@ public class HomeController {
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.IMAGE_JPEG); 
         headers.add("Content-Disposition", "inline;"); 
-        String filePath = file;
-        FileInputStream fis = new FileInputStream(filePath);
+
+        
+        String filePath = "";
+        InputStream fis = null;
+        if( file.contains("img/")) {
+            // 프로젝트 내의 이미지
+            filePath = "static/" + file;
+
+            // 클래스 패스 상의 리소스로부터 InputStream 얻기
+            Resource resource = new ClassPathResource(filePath);
+            fis = resource.getInputStream();
+        }
+
+        if( file.contains(":/") ) {
+            // 파일 시스템 이미지 (window)
+            File fileObj = new File(file);
+            fis = new FileInputStream(fileObj);
+        }
+
+
+        // FileInputStream fis = new FileInputStream(filePath);
         ServletOutputStream sos =  response.getOutputStream();
         FileCopyUtils.copy(fis, sos);
     }
